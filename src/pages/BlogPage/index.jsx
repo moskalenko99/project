1cdb934233ffe563aef174a7e620839bbc5e147c
asd <asd>
2020-02-25 19:02:16 +0200

    lesson 23

--- src/pages/BlogPage/index.jsx
+++ src/pages/BlogPage/index.jsx
@@ -5,11 +5,22 @@ import './styles.scss';
 
 class BlogPage extends React.Component{
 
+    section = React.createRef();
+
     state = {
         list: []
     };
 
+    constructor(props){
+        super(props);
+
+        console.log("CONSTRUCTOR", this.section);
+        
+    }
+
     componentDidMount() {
+        console.log('REF', this.section);
+        
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.json())
             .then(list => this.setState({ list }))
@@ -19,9 +30,10 @@ class BlogPage extends React.Component{
         return(
             <React.Fragment>
                 <h1>BlogPage</h1>
-                <section>
+                <section ref={this.section}>
                     {this.state.list.map((post, index) => (
                         <PostPreview 
+                            key={`PostPreview-${post.title}-${index}`}
                             title={post.title} 
                             description={post.body}
                             index={index} 
